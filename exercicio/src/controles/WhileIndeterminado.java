292e1fd13013e0b1f118e5d701237998405a8bfd
Davi Ferreira da Silva de Oliveira <daviferreira0106@gmail.com>
2026-08-27 21:19:36 -0300

    Resolvendo conflitos

--- exercicio/src/controles/WhileIndeterminado.java
+++ exercicio/src/controles/WhileIndeterminado.java
@@ -1,4 +1,17 @@
 package controles;
 
+import java.util.Scanner;
+
 public class WhileIndeterminado {
+    public static void main(String[] args) {
+        String exit = "";
+        Scanner sc = new Scanner(System.in);
+
+        while(!"sair".equalsIgnoreCase(exit)) {
+            System.out.print("Digite algo: ");
+            exit = sc.nextLine().toLowerCase();
+        }
+
+        sc.close();
+    }
 }
