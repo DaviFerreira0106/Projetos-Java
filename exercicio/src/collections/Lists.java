737a20742b0460b1e73da91c1e47ad60c127b3ab
Davi Ferreira da Silva de Oliveira <daviferreira0106@gmail.com>
2026-09-09 21:27:40 -0300

    Trabalhando com arraylists

--- exercicio/src/collections/Lists.java
+++ exercicio/src/collections/Lists.java
@@ -5,5 +5,26 @@ public class Lists {
 
     public static void main(String[] args) {
         ArrayList<ListsUsuario> listaUsuario = new ArrayList<ListsUsuario>();
+
+        listaUsuario.add(new ListsUsuario("Davi Ferreira"));
+        listaUsuario.add(new ListsUsuario("Carlito Ferreira"));
+        listaUsuario.add(new ListsUsuario("Carlos Luiz"));
+        listaUsuario.add(new ListsUsuario("Geovanio Souza"));
+        listaUsuario.add(new ListsUsuario("Pedro Silva"));
+        listaUsuario.add(new ListsUsuario("Davi Ferreira"));
+
+        System.out.println(listaUsuario.getFirst().nome);
+        System.out.println(listaUsuario.get(2).nome);
+        System.out.println(listaUsuario.getLast().nome);
+
+        System.out.println(listaUsuario.get(0).equals(listaUsuario.getLast()));
+        System.out.println(listaUsuario.getFirst());
+        System.out.println(listaUsuario.size());
+
+        System.out.println(listaUsuario.remove(new ListsUsuario("Carlos Luiz")));
+        ListsUsuario objRemovido = listaUsuario.remove(1);
+        System.out.println(objRemovido.nome);
+        System.out.println(listaUsuario.size());
+
     }
 }
--- exercicio/src/collections/ListsUsuario.java
+++ exercicio/src/collections/ListsUsuario.java
@@ -7,4 +7,18 @@ public class ListsUsuario {
     ListsUsuario(String nome) {
         this.nome = nome;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof ListsUsuario) {
+            return this.nome.equals(((ListsUsuario) obj).nome);
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Meu nome é " + this.nome + ".";
+    }
 }
